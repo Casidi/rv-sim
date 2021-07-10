b3443f2b3661ccc675b523b327bcb63fc381fb78
JCChange@HC <tcfsh22215@gmail.com>
2021-07-10 08:28:32 +0800

    Add basic core data structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Default)]
+struct RVCore {
+    pc: u32,
+    regs: [u32; 32],
+}
+
+impl RVCore {
+    fn step(&mut self, inst: u32) {
+        println!("PC = {}, inst = {}", self.pc, inst);
+        self.pc += 4;
+    }
+    
+    fn run(&mut self, num_steps: i32) {
+        let mut step_count = 0;
+        while step_count < num_steps {
+            self.step(0);
+            step_count += 1;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut core: RVCore = Default::default();
+    core.run(5);
 }
